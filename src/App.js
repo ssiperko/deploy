44bf36d0d0bc20a4ffba01e46c0df9b5bdb2dd6b
ssiperko <s.siperko@gmail.com>
2017-10-19 10:29:21 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import IndexItem from './components/IndexItem';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <h3>Howdy there</h3>
+          </div>
+    );
+  }
+}
+
+export default App;
