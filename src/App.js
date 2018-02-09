9bcf8844304f0c166ea96ae17288f8b06172d9d7
ssiperko <s.siperko@gmail.com>
2018-02-09 12:43:06 -0500

    new files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import IndexItem from './components/IndexItem';
-
-
 class App extends Component {
   render() {
     return (
         <div>
-          <h3>Howdy there</h3>
+          <h3>Hello World</h3>
           </div>
     );
   }
 }
-
 export default App;
