97a29b32f82b4ecbd5af880a48ca26f283a306ed
ssiperko <s.siperko@gmail.com>
2018-02-21 12:13:13 -0500

    style changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,12 +5,12 @@ export class Contact extends React.Component{
         return(
           <div className='contact'>
             <h2 className='headline'>If you are interested in my work, you may reach me at:</h2>
-            <h4 className='headline'>s.siperko@gmail.com</h4>
+            <h4 className='headline'>steven@steven-siperko.space</h4>
             <div className='contactIcons'>
             <button className='icon'><a href="https://github.com/ssiperko"><i className="fa fa-github buttonIcon" ></i></a></button>
             <button className='icon'><a href="https://www.linkedin.com/in/steven-siperko-0388325b/"><i className="fa fa-linkedin buttonIcon" ></i></a></button>
             <button className='icon'><a href="https://twitter.com/StevenSiperko"><i className="fa fa-twitter buttonIcon" ></i></a></button>
-            <button className='icon'><a href="mailto:s.siperko@gmail.com"><i className="fa fa-envelope buttonIcon" ></i></a></button>
+            <button className='icon'><a href="mailto:steven@steven-siperko.space"><i className="fa fa-envelope buttonIcon" ></i></a></button>
             </div>
           </div>
         );
