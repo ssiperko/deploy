eca1500030768a48d5af727bce641ae5e9ee76b8
ssiperko <s.siperko@gmail.com>
2018-02-26 08:34:06 -0500

    style

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,11 @@ import {Link} from 'react-router-dom';
 export class Home extends React.Component{
     render(){
         return(
-            <div className='headline'>
-                <h1>Steven Siperko</h1>
-                <Link to ='./projects'><h3 className="port">portfolio</h3></Link>
+          <div>
+                <div className='headline'>
+                    <h1>Steven Siperko</h1>
+                    <Link to ='./projects'><h3 className="port">portfolio</h3></Link>
+                </div>
             </div>
         );
     }
--- src/Components/ProjectDisplay.js
+++ src/Components/ProjectDisplay.js
@@ -21,20 +21,6 @@ export class Display extends React.Component{
                             <Link to = './wiki' className='link'><div className="slide">An app that allows users to quickly access Wikipedia entries without going to the Wikipedia page.</div></Link>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col">
-                            <div className="photo-container"><img src="https://s19.postimg.org/r0435mcqb/Screen_Shot_2017-12-14_at_4.32.09_PM.png" alt="calculator"/></div>
-                          <Link to = './calc' className='link'> <div className="slide">An app that mimics the functionality and apperance of the standard, scientific calculator</div></Link>
-                        </div>
-                        <div className="col">
-                            <div className="photo-container"><img src="https://s19.postimg.org/eze1r0xwj/Screen_Shot_2017-12-14_at_2.57.26_PM.png" alt="weather app"/></div>
-                            <Link to = './weather' className='link'><div className="slide">An app that allows users to view weather conditions in whatever city they elect.</div></Link>
-                        </div>
-                        <div className="col">
-                            <div className="photo-container"><img src="https://s19.postimg.org/50xoi58o3/Screen_Shot_2017-12-14_at_4.24.21_PM.png" alt="wikipedia viewer"/></div>
-                            <Link to = './wiki' className='link'><div className="slide">An app that allows users to quickly access Wikipedia entries without going to the Wikipedia page.</div></Link>
-                        </div>
-                    </div>
                     <hr />
                 </div>
             </section>
